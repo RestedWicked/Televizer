6375913608b1b25bd2e98675fd04ffbd2812872a
Jesus Perez <jesusdperez02@gmail.com>
2022-11-27 00:23:45 -0800

    refactor(EntireProject): Finally found a starting point I wanted

---
+++ src/main/java/xyz/restedwickeddev/televizer/Televizer.kt
@@ -0,0 +1,14 @@
+package xyz.restedwickeddev.televizer
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Televizer : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+        
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
