470e028129b4aaa78e941a4265c64b4ef4ee787e
Jesus Perez <jesusdperez02@gmail.com>
2022-11-27 01:53:49 -0800

    feat(Boot Messages): Added Messages, and Logo to console :3

--- src/main/java/xyz/restedwickeddev/televizer/Televizer.kt
+++ src/main/java/xyz/restedwickeddev/televizer/Televizer.kt
@@ -1,14 +1,24 @@
 package xyz.restedwickeddev.televizer
 
+import org.bukkit.ChatColor
 import org.bukkit.plugin.java.JavaPlugin
 
 class Televizer : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
-        
+        logger.info("${ChatColor.GREEN}Enabled ${this.name}")
+        logger.info("${ChatColor.DARK_AQUA}______${ChatColor.LIGHT_PURPLE}___________")
+        logger.info("${ChatColor.DARK_AQUA}|     \\${ChatColor.LIGHT_PURPLE}         /")
+        logger.info("${ChatColor.DARK_AQUA}|      \\${ChatColor.LIGHT_PURPLE}       /")
+        logger.info("${ChatColor.DARK_AQUA}|     /${ChatColor.LIGHT_PURPLE}       /")
+        logger.info("${ChatColor.DARK_AQUA}|     \\${ChatColor.LIGHT_PURPLE}  /\\  /")
+        logger.info("${ChatColor.DARK_AQUA}|______\\${ChatColor.LIGHT_PURPLE}/  \\/")
+        logger.info("${ChatColor.DARK_AQUA}Wicked was here!")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info("Bye Bye! :3")
+        logger.info("${ChatColor.RED}Disabled ${this.name}")
     }
 }
